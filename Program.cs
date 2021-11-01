fe95b98d5de2c959810f6754b82faa6591f16c69
Maurício Santos <edutran.ms.98@gmail.com>
2021-11-01 16:22:41 -0300

    estudando csharp

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace estudandocsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 4;
+
+            if (a == 1)
+            {
+                Console.WriteLine("entrou na condição do if");
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine("entrou na condição 2");
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine("entrou na condição 3");
+            }
+            else
+            {
+                Console.WriteLine("entrou na condição do else");
+            }
+
+            var x = 2;
+            string xy = (x == 1 + 1 ? "sim" : "não");
+            Console.WriteLine(xy);
+
+            var y = 1;
+            while (y < 5)
+            {
+                Console.WriteLine(y);
+                y = y + 1;
+            }
+        }
+    }
+}
