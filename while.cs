8d3c49fd5707b8e8f4f0a7df037afaf247213368
Maurício Santos <edutran.ms.98@gmail.com>
2021-11-01 19:25:45 -0300

    while

---
+++ while.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace estudandocsharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+
+            while (a <= 10)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+        }
+    }
+}
